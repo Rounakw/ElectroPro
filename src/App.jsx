bd8f8b33555b1ba3c008532801d250195502e08e
Rounak Basak <rounakbasak12@gmail.com>
2024-04-03 10:35:06 +0530

    Add Navlink features

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 
 import './App.css'
+
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Footer from './assets/components/AllPages/Home/Footer/Footer';
 import Home from './assets/components/AllPages/Home/Home';
 import Navbar from './assets/components/Navbar/Navbar';
+import Layout from './assets/components/Layout/Layout';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='ElectroPro/' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
+        <Route path='home' element={<Home/>}/>
+        <Route path='product' element={<h1>Product page</h1>}/>
+        <Route path='cart' element={<h1>cart empty</h1>}/>
+        <Route path='contact' element={<h1>Contact page unavailable</h1>}/>
+      </Route>
+
+    )
+  )
 
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+    <RouterProvider router={router}/>
     </>
   )
 }
--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -7,8 +7,9 @@ import home from "./assets/home.png"
 import product from "./assets/product.png"
 import cart from "./assets/cart.png"
 import contact from "./assets/contact.png"
+import { NavLink } from 'react-router-dom'
 function Navbar() {
-let totalCart = 0
+    let totalCart = 0
 
     const [show, setShow] = useState(false)
     function handleToggleMenu() {
@@ -16,41 +17,59 @@ let totalCart = 0
     }
     return (
         <>
-        <div className='navbar'>
-            <Logo />
-            <div className='navbarpageslist'>
-                <p className='navitem-home'>Home</p>
-                <p className='navitem-produc'>Product</p>
-                <p className='navitem-cart'>
-                    Cart
-                <span className='totalcart'>{totalCart}</span></p>
-                
-                <p className='navitem-contact'>Contact us</p>
-                
-            </div>
-            <div className="mobile-right">
-                <div className='navbar-signin'>
-                    <button >Sign In</button>
+            <div className='navbar'>
+                <NavLink to={''}>
+                    <Logo />
+                </NavLink>
+
+                <div className='navbarpageslist'>
+                    <p className='navitem-home'>
+                        <NavLink to={"home"} style={({ isActive }) => { return { color: isActive ? "red" : "white", textDecoration: "none" } }}>
+                            Home
+                        </NavLink>
+                    </p>
+
+                    <p className='navitem-produc'>
+                        <NavLink to={"product"} style={({ isActive }) => { return { color: isActive ? "red" : "white", textDecoration: "none" } }}>Product
+                        </NavLink>
+                    </p>
+                    <p className='navitem-cart'>
+                        <NavLink to={"cart"} style={({ isActive }) => { return { color: isActive ? "red" : "white", textDecoration: "none" } }}>
+                            Cart
+                        </NavLink>
+                        <span className='totalcart'>{totalCart}</span>
+                    </p>
+
+                    <p className='navitem-contact'>
+                    <NavLink to={"contact"} style={({ isActive }) => { return { color: isActive ? "red" : "white", textDecoration: "none" } }}>
+                        Contact us
+                        </NavLink>
+                        </p>
+
+                </div>
+                <div className="mobile-right">
+                    <div className='navbar-signin'>
+                        <button >Sign In</button>
+                    </div>
+
+                    <div className='navbar-hamburger-menu'>
+                        <img src={show ? cross : menu} onClick={handleToggleMenu} alt="" />
+                    </div>
                 </div>
 
-                <div className='navbar-hamburger-menu'>
-                    <img src={show ? cross : menu} onClick={handleToggleMenu} alt="" />
+                <div className={show ? 'hamburgermenu-slider' : 'hidden'}>
+                    <br />
+                    <div className='hamburger-menu-icon'>
+                        <Logo />
+                    </div>
+                    <br />
+                    <p className='navitem-home'><img src={home} alt="" />Home</p>
+                    <p className='navitem-product'><img src={product} alt="" />Product</p>
+                    <p className='navitem-cart' style={{ position: "relative" }}><img src={cart} alt="" />Cart
+                        <span className='totalcart'>{totalCart}</span></p>
+                    <p className='navitem-contact'><img src={contact} alt="" />Contact us</p>
                 </div>
             </div>
-
-            <div className={show?'hamburgermenu-slider':'hidden'}>
-                <br />
-                <div className='hamburger-menu-icon'>
-                <Logo />
-                </div>
-                <br />
-                <p className='navitem-home'><img src={home} alt="" />Home</p>
-                <p className='navitem-product'><img src={product} alt="" />Product</p>
-                <p className='navitem-cart' style={{position:"relative"}}><img src={cart} alt="" />Cart
-                <span className='totalcart'>{totalCart}</span></p>
-                <p className='navitem-contact'><img src={contact} alt="" />Contact us</p>
-            </div>
-        </div>
         </>
     )
 }
